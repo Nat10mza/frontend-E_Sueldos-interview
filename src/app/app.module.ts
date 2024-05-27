354c1dfcfdd6e6a787e9d96bf8bdd793eae63f24
nat10mza <nico.tramontina@gmail.com>
2024-05-27 05:39:45 -0300

    feat: login register and user admin interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserCreationComponent } from './components/user-creation/user-creation.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    UserCreationComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
