72d0ba77ad80022ec55d82ea6bb39cd8c1f514b4
nat10mza <nico.tramontina@gmail.com>
2024-05-28 03:05:56 -0300

    feat: initial form state

--- src/app/components/login/login-form/login-form.component.ts
+++ src/app/components/login/login-form/login-form.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+  submitted = false;
 
+  constructor(public dialogRef: MatDialogRef<LoginFormComponent>) {}
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);
+    this.submitted = true;
+    f.reset();
+    console.log(this.submitted);
+    this.submitted = false;
+    console.log(this.submitted);
+    this.dialogRef.close();
+  }
 }
--- src/app/components/register/register-form/register-form.component.ts
+++ src/app/components/register/register-form/register-form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.css']
+  styleUrls: ['./register-form.component.css'],
 })
 export class RegisterFormComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  submitted = false;
 
+  constructor(public dialogRef: MatDialogRef<RegisterFormComponent>) {}
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);
+    this.submitted = true;
+    f.reset();
+    console.log(this.submitted);
+    this.submitted = false;
+    console.log(this.submitted);
+    this.dialogRef.close();
+  }
 }
